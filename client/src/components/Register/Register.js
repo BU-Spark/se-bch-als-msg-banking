34287b169a5de9011936d2904880306426e632bf
Daniel Delijani <danieldelijani@gmail.com>
2022-10-17 19:01:24 -0400

    Email and password verification in-form implemented

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -7,13 +7,43 @@ import "./Register.css";
 
 function Register() {
   const [email, setEmail] = useState("");
+  const [confirmEmail, setConfirmEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
   const [name, setName] = useState("");
   const [user, loading] = useAuthState(auth);
   const history = useHistory();
+
+  const validateEmail = e => {
+    if (email === confirmEmail) {
+      setEmailError("");
+    }
+    else {
+      setEmailError("Emails do not match.");
+    }
+  }
+
+  const validatePassword = e => {
+    if (password === confirmPassword) {
+      setPasswordError("");
+    }
+    else {
+      setPasswordError("Passwords do not match.");
+    }
+  }
+
   const register = () => {
-    if (!name) alert("Please enter name");
-    registerWithEmailAndPassword(name, email, password);
+    if (!name) {
+      alert("Please enter name");
+    } else if (emailError) {
+      alert("Email and confirmation email do not match.");
+    } else if (passwordError) {
+      alert("Password and confirmation password do not match.");
+    } else {
+      registerWithEmailAndPassword(name, email, password);
+    }
   };
   useEffect(() => {
     if (loading) return;
@@ -42,12 +72,30 @@ function Register() {
             placeholder="E-mail Address"
           />
           <input
+            type="text"
+            className="register__textBox"
+            value={confirmEmail}
+            onChange={(e) => setConfirmEmail(e.target.value)}
+            placeholder="Confirm E-mail"
+            onBlur={validateEmail}
+          />
+          {emailError && <span className='err'>{emailError}</span>}
+          <input
             type="password"
             className="register__textBox"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
           />
+          <input
+            type="password"
+            className="register__textBox"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            placeholder="Confirm Password"
+            onBlur = {validatePassword}
+          />
+          {passwordError && <span className='err'>{passwordError}</span>}
           <button className="register__btn" onClick={register}>
             Register
           </button>
