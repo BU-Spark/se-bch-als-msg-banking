2bf59922adac9f88e778495ee961dab6f705e828
shaolin-x <xshaolin123@gmail.com>
2022-12-03 19:46:35 -0500

    looks great

--- client/src/components/Retrieve/Retrieve.js
+++ client/src/components/Retrieve/Retrieve.js
@@ -35,6 +35,88 @@ function Retrieve() {
     if (!user) return history.replace(process.env.PUBLIC_URL + "/");
     fetchUserAudio();
   }, [loading]);
+  function showLoading() {
+    if (document.getElementById("divLoadingFrame") != null) {
+      return;
+    }
+    var style = document.createElement("style");
+    style.id = "styleLoadingWindow";
+    style.innerHTML = `
+          .loading-frame {
+              position: fixed;
+              background-color: rgba(0, 0, 0, 0.8);
+              left: 0;
+              top: 0;
+              right: 0;
+              bottom: 0;
+              z-index: 4;
+          }
+  
+          .loading-track {
+              height: 50px;
+              display: inline-block;
+              position: absolute;
+              top: calc(50% - 50px);
+              left: 50%;
+          }
+  
+          .loading-dot {
+              height: 5px;
+              width: 5px;
+              background-color: white;
+              border-radius: 100%;
+              opacity: 0;
+          }
+  
+          .loading-dot-animated {
+              animation-name: loading-dot-animated;
+              animation-direction: alternate;
+              animation-duration: .75s;
+              animation-iteration-count: infinite;
+              animation-timing-function: ease-in-out;
+          }
+  
+          @keyframes loading-dot-animated {
+              from {
+                  opacity: 0;
+              }
+  
+              to {
+                  opacity: 1;
+              }
+          }
+      `
+    document.body.appendChild(style);
+    var frame = document.createElement("div");
+    frame.id = "divLoadingFrame";
+    frame.classList.add("loading-frame");
+    for (var i = 0; i < 10; i++) {
+      var track = document.createElement("div");
+      track.classList.add("loading-track");
+      var dot = document.createElement("div");
+      dot.classList.add("loading-dot");
+      track.style.transform = "rotate(" + String(i * 36) + "deg)";
+      track.appendChild(dot);
+      frame.appendChild(track);
+    }
+    document.body.appendChild(frame);
+    var wait = 0;
+    var dots = document.getElementsByClassName("loading-dot");
+    // There was a warning i is already defined, thus use j for index instead.
+    for (var j = 0; j < dots.length; j++) {
+      window.setTimeout(function(dot) {
+        dot.classList.add("loading-dot-animated");
+      }, wait, dots[j]);
+      wait += 150;
+    }
+  };
+
+  function removeLoading() {
+    document.body.removeChild(document.getElementById("divLoadingFrame"));
+    document.body.removeChild(document.getElementById("styleLoadingWindow"));
+  };
+
+
   const downloadClip = async (name) => {
     try {
       const response = await axios.post(
@@ -50,7 +132,7 @@ function Retrieve() {
   };
   const deleteUnprocessedAudio = async (cloudStorageFileName) => {
     try {
-      console.log(cloudStorageFileName)
+      showLoading();
       const token = await auth.currentUser.getIdToken();
       const response = await axios.delete(
         (url + "/delete_unprocessed_audio"),
@@ -60,11 +142,18 @@ function Retrieve() {
             'Authorization': token
           }
         }
-      );
-      console.log('Response from delete audio:');
-      console.log(response)
+      ).then( response => {
+        console.log('Response from delete audio:');
+        console.log(response);
+        removeLoading();
+
+        window.location.reload(false);
+      }
+      )
+
     } catch (err) {
       console.log(err);
+      removeLoading();
     }
   };
   const deleteProcessedAudio = async (cloudStorageFileName) => {
@@ -154,28 +243,22 @@ function Retrieve() {
   }
 
 
-//   const clickhandler = (audio_name)=>{
-//     downloadClip(audio_name);
-//  }
 
   return (
     <>
       <h1 className="dashboard-header text-center">Rediscover Your Voice</h1>
-      <h2 className="dashboard-header text-center">Click to Download!</h2>
       <table>
         <thead>
           <tr>
-            <th className="uk-table-shrink">Name</th>
-            <th>Files</th>
-            <th>Date</th>
-            <th colspan="2" align="right">Actions</th>
-          </tr>
-          
+            <th align="center"> Name </th>
+            <th> Files </th>
+            <th> Date </th>
+            <th colspan="2"> Actions </th>
+          </tr>        
         </thead>
 
         {audio.map((value, index) => {
           var db_name = "";
-          var audio_name = "";
           db_name = Object.keys(value).map(inner => {
             // let db = inner; //audio name
             let db = value[inner];  //database name
@@ -193,14 +276,13 @@ function Retrieve() {
    
           return (
             <tbody>
-            <tr align="right">
-              <td><Collapsible index={index} /> </td>
-              <td align="right">    {}     </td>
-              <td align="right"> {}</td>
-              <td align="right">  <button className="button" onClick={() => downloadClip(db_name)}>Download</button> </td>
-              {/* <td align="right">  <button className="button" onClick={clickhandler()}>Download</button> </td> */}
-              <td aligb = "left"> <button className="button" onClick={() => deleteUnprocessedAudio(db_name)}>Delete</button> </td>
-
+              <tr align="right">
+                <td className = "table-first-col"><Collapsible index={index} /> </td>
+                <td align="right">    {1}     </td>
+                <td align="right"> {1}</td>
+                <td align="right">  <button className="button" onClick={() => downloadClip(db_name)}>Download</button> </td>
+                {/* <td aligb = "left"> <button className="button" onClick={() => deleteUnprocessedAudio(db_name)}>Delete</button> </td> */}
+                <td > <button className="button" onClick={() => {deleteUnprocessedAudio(db_name);  }}>Delete</button> </td>
             </tr>
             </tbody>
           );
